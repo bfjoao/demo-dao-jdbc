91147fec9cfd33b3a64e0ce52e3135dc84d52b02
João Barbosa <joao.bffff@gmail.com>
2025-02-21 19:36:08 -0300

    Implementação do FindById

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -14,6 +14,12 @@ import model.entidades.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -34,7 +40,39 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT seller.*, department.Name as DepName "
+										+ "FROM seller INNER JOIN department " 
+										+ "ON seller.DepartmentId = departmentId "
+										+ "WHERE seller.Id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			if (rs.next()) { // Testa se veio algum resultado na consulta
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setNome(rs.getString("DepName"));
+				Seller sell = new Seller();
+				sell.setId(rs.getInt("Id"));
+				sell.setNome(rs.getString("Name"));
+				sell.setEmail(rs.getString("Email"));
+				sell.setBaseSalary(rs.getDouble("BaseSalary"));
+				sell.setBirthDate(rs.getDate("BirthDate"));
+				sell.setDepartment(dep);
+				return sell;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
