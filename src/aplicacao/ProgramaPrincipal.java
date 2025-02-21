a69e9ab474285b5d985aede5173300b5ccd83abc
João Pedro Barbosa Fernandes <joao-fernandes@REDERS>
2025-02-21 10:30:56 -0300

    Classe da entidade Seller

--- src/aplicacao/ProgramaPrincipal.java
+++ src/aplicacao/ProgramaPrincipal.java
@@ -1,6 +1,9 @@
 package aplicacao;
 
+import java.util.Date;
+
 import model.entidades.Department;
+import model.entidades.Seller;
 
 public class ProgramaPrincipal {
 
@@ -9,6 +12,9 @@ public class ProgramaPrincipal {
 
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
