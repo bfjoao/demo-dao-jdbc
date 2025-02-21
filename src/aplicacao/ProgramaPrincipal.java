c5e9cee728bdab7453db84e709b0095fab63f4de
João Pedro Barbosa Fernandes <joao-fernandes@REDERS>
2025-02-21 10:18:38 -0300

    Classe da entidade Department

---
+++ src/aplicacao/ProgramaPrincipal.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import model.entidades.Department;
+
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+	}
+
+}
