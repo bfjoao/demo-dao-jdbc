b0cb3ca2d3a96cebd58b57cf3e584a12ae8b499e
João Barbosa <joao.bffff@gmail.com>
2025-02-21 19:28:13 -0300

    SellerDaoJDBC e DaoFactory

--- src/aplicacao/ProgramaPrincipal.java
+++ src/aplicacao/ProgramaPrincipal.java
@@ -2,6 +2,8 @@ package aplicacao;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entidades.Department;
 import model.entidades.Seller;
 
@@ -11,9 +13,10 @@ public class ProgramaPrincipal {
 		// TODO Auto-generated method stub
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
+
 		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,45 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.SellerDao;
+import model.entidades.Department;
+import model.entidades.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
